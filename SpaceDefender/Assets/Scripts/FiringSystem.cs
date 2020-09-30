feb926af5fec41c03df6922b2bc94c98c664b65d
Dmitry Pyzhov <fiche40@gmail.com>
2020-09-30 16:21:02 +0300

    New Firing System & Updated Upgrade UI

---
+++ SpaceDefender/Assets/Scripts/FiringSystem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class FiringSystem {
+    public bool isActive = false;
+    public string systemName;
+    public GameObject projectilePrefab;
+    public List<Transform> firePoints;
+    public float projectileSpeed = 10f;
+    public float projectileFiringPeriod = .15f;
+
+    public float projectileFiringPeriodCounter = 0f;
+}
