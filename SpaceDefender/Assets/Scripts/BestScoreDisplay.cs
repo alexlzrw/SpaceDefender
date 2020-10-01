d02f8a33165ffe72e64b7217fcb552aad10def6e
Alex <alexlzrw@gmail.com>
2020-10-01 12:52:14 +0300

    Liderboard

---
+++ SpaceDefender/Assets/Scripts/BestScoreDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BestScoreDisplay : MonoBehaviour {
+
+    TextMeshProUGUI bestScoreText;
+    GameSession gameSession;
+
+    void Start() {
+        bestScoreText = GetComponent<TextMeshProUGUI>();
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    void Update() {
+        bestScoreText.text = gameSession.BestScore().ToString("000 000 000");
+    }
+}
